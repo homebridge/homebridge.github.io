64d850dfe4864cbb1c0ae37352e622b44d6961de
oznu <dev@oz.nu>
2020-05-02 15:42:24 +1000

    close sidebar after navigation on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HapService } from './hap.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,22 @@ export class AppComponent {
 
   constructor(
     public hapService: HapService,
-  ) {}
+    router: Router,
+  ) {
+
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (window.innerWidth >= 1200) {
+          return;
+        }
+        const sidebar = document.getElementById('docs-sidebar');
+        if (sidebar && sidebar.classList.contains('sidebar-visible')) {
+          sidebar.classList.remove('sidebar-visible');
+          sidebar.classList.add('sidebar-hidden');
+        }
+      }
+    });
+
+  }
 
 }
